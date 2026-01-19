d60c6901e5ddbdaa5406d970e276538a48eb0f03
psevdonimux <mail_451@mail.ru>
2026-01-19 20:08:25 +0300

    Обновлён installer.sh

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 apt update && apt upgrade -y
 apt install git php php-pdo mariadb -y
 mysql_install_db
-git clone https://github.com/USER/REPO.git clicker
+git clone https://github.com/psevdonimux/livdest-of-clicks-battle.git clicker
 cd clicker
 cp .env.example .env
-echo "Done! Edit clicker/.env with your database credentials"
+
